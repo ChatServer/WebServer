221e603e5bf63ecd3f5e58127e86026681f63665
Buhrkall <Buhrkall@10.50.137.223>
2014-09-09 14:21:21 +0200

    simple webserver added, with basic html pages.

--- WebServer/src/webserver/WebServer.java
+++ WebServer/src/webserver/WebServer.java
@@ -6,6 +6,16 @@
 
 package webserver;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 /**
  *
  * @author Ejer
@@ -15,14 +25,97 @@ public class WebServer {
     static int port = 8080; //not the final port
     static String ip =  "127.0.0.1"; // not the final IP
     
-    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        InetSocketAddress i = new InetSocketAddress(ip, port); //localhost - 127.0.0.1
+        HttpServer server = HttpServer.create(i,0);
+        server.createContext("/startpage", new PagesHandler());  
+        server.createContext("/online" , new OnlineHandler());
+        server.createContext("/logfile" , new LogFileHandler());
+
+        server.setExecutor(null);
+        server.start();
         
-       
     }
+    
+    static class PagesHandler implements HttpHandler {
+
+            String content = "public/";
+
+            @Override
+            public void handle(HttpExchange he) throws IOException {
+
+                File file = new File(content + "index.html");
+
+                byte[] bytesToSend = new byte[(int) file.length()];
+                try {
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                    bis.read(bytesToSend, 0, bytesToSend.length);
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+                he.sendResponseHeaders(200, bytesToSend.length);
+                try (OutputStream os = he.getResponseBody()) {
+                    os.write(bytesToSend, 0, bytesToSend.length);
+                }
+
+            }
+
+        }
+    
+    
+
+    static class OnlineHandler implements HttpHandler {
+
+            String content = "public/";
+
+            @Override
+            public void handle(HttpExchange he) throws IOException {
+
+                File file = new File(content + "html2.html");
+
+                byte[] bytesToSend = new byte[(int) file.length()];
+                try {
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                    bis.read(bytesToSend, 0, bytesToSend.length);
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+                he.sendResponseHeaders(200, bytesToSend.length);
+                try (OutputStream os = he.getResponseBody()) {
+                    os.write(bytesToSend, 0, bytesToSend.length);
+                }
+
+            }
+
+        }
+    
+    static class LogFileHandler implements HttpHandler {
+
+            String content = "public/";
+
+            @Override
+            public void handle(HttpExchange he) throws IOException {
+
+                File file = new File(content + "html2.html");
+
+                byte[] bytesToSend = new byte[(int) file.length()];
+                try {
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                    bis.read(bytesToSend, 0, bytesToSend.length);
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
+                he.sendResponseHeaders(200, bytesToSend.length);
+                try (OutputStream os = he.getResponseBody()) {
+                    os.write(bytesToSend, 0, bytesToSend.length);
+                }
+
+            }
+
+        }
     
 }
